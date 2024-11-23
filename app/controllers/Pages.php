8212cafa566969d5d94f2402c580adbd0320b9ff
Chiseli Iuri <ghost.rogue22@gmail.com>
2024-11-23 15:48:32 +0200

    feat(methods funtctionality): add method loading and calling funtionality

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -1,7 +1,15 @@
 <?php
     class Pages{
         public function __construct(){
-            echo 'Pages loaded';
+
+        }
+
+        public function index(){
+            
+        }
+
+        public function about($id3){
+            print_r($id3);
         }
     }
     
