2c991b7dd507e2ac13c0f6c0308324bc8f4b08a2
Chiseli Iuri <ghost.rogue22@gmail.com>
2024-11-24 20:37:38 +0200

    feat(Config File&Uploader): develop Config File and Uploader

---
+++ app/config/config.php
@@ -0,0 +1,7 @@
+<?php
+    //App Root
+    define('APPROOT', dirname(dirname(__FILE__)));
+    //URL Root
+    define('URLROOT', 'http://traversy.local');
+    //Sitte name
+    define('SITENAME', 'TraversyMVC');
