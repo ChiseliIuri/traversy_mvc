a42dbb2fc626182a8da0cf59fb912c69dbd4c9c5
Chiseli Iuri <ghost.rogue22@gmail.com>
2024-11-26 21:45:39 +0200

    chore(finish): clean up the development config data

--- app/config/config.php
+++ app/config/config.php
@@ -1,7 +1,13 @@
 <?php
+    //DB params
+    define('DB_HOST', '127.0.0.1');
+    define('DB_USER', '_YOUR_USER_');
+    define('DB_PASS', '_YOUR_PASS_');
+    define('DB_NAME', '_YOUR_DBNAME_');
+
     //App Root
     define('APPROOT', dirname(dirname(__FILE__)));
     //URL Root
-    define('URLROOT', 'http://traversy.local');
+    define('URLROOT', '_YOUR_URL_');
     //Sitte name
-    define('SITENAME', 'TraversyMVC');
+    define('SITENAME', '_YOUR_NAME_');
